bd7248cc110b120c47ecc4df0266b7a85e65569f
gabriel <gmorigamer@gmail.com>
2023-07-06 21:01:57 -0300

    Adiciona arquivo

--- README copy.md
+++
@@ -1,2 +0,0 @@
-# clone-tabnews-cursodev
-Na jornada para a carreira de desenvolvedor no cursodev.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você é fera, José 😎</h1>
+  return <h1>Você é fera, José 😎</h1>
 }
 
 export default Home;

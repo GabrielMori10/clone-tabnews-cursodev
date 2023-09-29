c58996895ad9f4d8b405b1cd9b328626c0cac953
Gabriel Mori <gabriel@DESKTOP-C3G6LIO>
2023-09-28 22:08:59 -0300

    piada interna

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 function Home() {
   return (
-    <h1 style={{ color: "DarkOliveGreen", fontFamily: "Arial" }}>
-      Está faltando secretos na casa de sisighost
-    </h1>
+    <div>
+      <h1>10 reasons why jesse pinkman is the hottest guy that ever lived</h1>
+      <p style={{ color: "#55CDFC" }}>number 1 - he is hot</p>
+      <p style={{ color: "#55CDFC" }}>number 2 - he makes meth</p>
+      <p style={{ color: "#F7A8B8" }}>number 3 - he plays minecraft with me</p>
+      <p style={{ color: "#F7A8B8" }}>number 4 - he smoke</p>
+      <p style={{ color: "#ffffff" }}>number 5 - he is jesse pinkman</p>
+      <p style={{ color: "#ffffff" }}>number 6 - he is my boyfriend</p>
+      <p style={{ color: "#F7A8B8" }}>number 7 - he drives</p>
+      <p style={{ color: "#F7A8B8" }}>number 8 - he listen to sick music</p>
+      <p style={{ color: "#55CDFC" }}>number 9 - he has a big dick</p>
+      <p style={{ color: "#55CDFC" }}>number 10 - he is literally on fire</p>
+    </div>
   );
 }
 

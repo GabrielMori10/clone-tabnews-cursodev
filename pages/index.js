d14af3efcb14107128bdcfca7bacc77a0f6415f9
Gabriel Mori <gabriel@DESKTOP-C3G6LIO>
2023-11-10 20:48:28 -0300

    add `databse.js` and `.env` files

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,5 @@
 function Home() {
-  return (
-    <div>
-      <h1>10 reasons why jesse pinkman is the hottest guy that ever lived</h1>
-      <p style={{ color: "#55CDFC" }}>number 1 - he is hot</p>
-      <p style={{ color: "#55CDFC" }}>number 2 - he makes meth</p>
-      <p style={{ color: "#F7A8B8" }}>number 3 - he plays minecraft with me</p>
-      <p style={{ color: "#F7A8B8" }}>number 4 - he smoke</p>
-      <p style={{ color: "#ffffff" }}>number 5 - he is jesse pinkman</p>
-      <p style={{ color: "#ffffff" }}>number 6 - he is my boyfriend</p>
-      <p style={{ color: "#F7A8B8" }}>number 7 - he drives</p>
-      <p style={{ color: "#F7A8B8" }}>number 8 - he listen to sick music</p>
-      <p style={{ color: "#55CDFC" }}>number 9 - he has a big dick</p>
-      <p style={{ color: "#55CDFC" }}>number 10 - he is literally on fire</p>
-    </div>
-  );
+  return <h1>As vezes é melhor beber café do que desisitir</h1>;
 }
 
 export default Home;

ca40849069779f325acfd30297847a7fdd447c03
Gabriel Mori <gabriel@DESKTOP-C3G6LIO>
2023-12-05 21:24:23 -0300

    Makes the database return the `version`, `max_connections` and `opened_connections`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,19 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ aaaa: "Eu amo café" });
+  const updatedAt = new Date().toISOString();
+  const maxConnections = await database.query("SHOW max_connections;");
+  const openedConnections = await database.query(
+    "SELECT * FROM pg_stat_activity;",
+  );
+  const version = await database.query("SHOW server_version;");
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    max_connections: maxConnections.rows[0].max_connections,
+    opened_connections: openedConnections.rows.length,
+    version: version.rows[0].server_version,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,23 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const respondeBody = await response.json();
+  console.log(respondeBody);
+
+  // Updated At
+  const parsedUpdatedAt = new Date(respondeBody.updated_at).toISOString();
+  expect(respondeBody.updated_at).toEqual(parsedUpdatedAt);
+
+  // Max Connections
+  expect(typeof respondeBody.max_connections).toBe("string");
+  expect(+respondeBody.max_connections).not.toBeNaN();
+  expect(+respondeBody.max_connections).toBeGreaterThan(0);
+
+  // Opened Connections
+  expect(typeof respondeBody.opened_connections).toBe("number");
+
+  // Postgres Version
+  expect(typeof respondeBody.version).toBe("string");
+  expect(+respondeBody.version).not.toBeNaN();
 });

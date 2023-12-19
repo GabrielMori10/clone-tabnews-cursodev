bf13bb73bf081a883dfedcc9526fb3a24a813149
Gabriel Mori <gabriel@DESKTOP-C3G6LIO>
2023-12-19 12:35:53 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,17 +2,33 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  const maxConnections = await database.query("SHOW max_connections;");
-  const openedConnections = await database.query(
-    "SELECT * FROM pg_stat_activity;",
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
   );
-  const version = await database.query("SHOW server_version;");
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
-    max_connections: maxConnections.rows[0].max_connections,
-    opened_connections: openedConnections.rows.length,
-    version: version.rows[0].server_version,
+    dependencies: {
+      database: {
+        max_connections: +databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
+        version: databaseVersionValue,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,21 +3,12 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const respondeBody = await response.json();
-  console.log(respondeBody);
+  // console.log(respondeBody);
 
-  // Updated At
   const parsedUpdatedAt = new Date(respondeBody.updated_at).toISOString();
   expect(respondeBody.updated_at).toEqual(parsedUpdatedAt);
 
-  // Max Connections
-  expect(typeof respondeBody.max_connections).toBe("string");
-  expect(+respondeBody.max_connections).not.toBeNaN();
-  expect(+respondeBody.max_connections).toBeGreaterThan(0);
-
-  // Opened Connections
-  expect(typeof respondeBody.opened_connections).toBe("number");
-
-  // Postgres Version
-  expect(typeof respondeBody.version).toBe("string");
-  expect(+respondeBody.version).not.toBeNaN();
+  expect(respondeBody.dependencies.database.version).toEqual("16.0");
+  expect(respondeBody.dependencies.database.max_connections).toEqual(100);
+  expect(respondeBody.dependencies.database.opened_connections).toEqual(1);
 });
